429846c048d7a3c8ae4ddf9b856c646ce32318fa
Joel Jerez <joel.jerez@git.com>
2025-02-23 23:34:35 -0500

    v1.1.2

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -2,9 +2,10 @@ export * from './Colors';
 export * from './DarkTheme';
 export * from './LightTheme';
 export * from './Styles';
-export * from './ThemeProvider';
 export * from './types';
 export * from './Typography';
+
+export { default as ThemeProvider, useThemeContext } from './ThemeProvider';
 export { default as useStyles } from './useStyles';
 export { default as useTheme } from './useTheme';
 
